29a9d1311d535cccfcbed1a0d8d3bb337c35c018
scoomp <moshnyakov.alexei@yandex.ru>
2023-08-13 13:01:13 +0300

    homework

--- src/HW/4/Button.tsx
+++ src/HW/4/Button.tsx
@@ -1,16 +1,17 @@
 type ButtonPropsType = {
-  callBack: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+  callBack: ()=>void // НУЖНО ПРОТИПИЗИРОВАТЬ
   name: any // НУЖНО ПРОТИПИЗИРОВАТЬ
 };
 
 export const Button = (props: ButtonPropsType) => {
   const callBackHandler = () => {
     // НУЖНО ДОПИСАТЬ
+    props.callBack()
   };
 
   return (
-    <button id={'hw04-button'} onClick={callBackHandler}>
-      {props.name}
-    </button>
+      <button id={'hw04-button'} onClick={callBackHandler}>
+        {props.name}
+      </button>
   );
 };
